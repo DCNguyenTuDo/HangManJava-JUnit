f0c2f2c72deb226175971a548814225636b28fe4
Duke TuDo <Duke TuDo@DESKTOP-VGNVEDM>
2018-03-26 10:26:44 +0200

    Eighth Commit

---
+++ src/udo/edu/test/GameTest.java
@@ -0,0 +1,27 @@
+package udo.edu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import udo.edu.game.Game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+	@Test 
+	void testGame() {
+		Game game1 = new Game(); // When a new Game is created / instantiated, then methods initGame() and loadQuestion() are also called
+        
+		assertNotNull(game1.answer, "Should be not empty");
+		assertNotNull(game1.question, "Should be not empty");
+		assertEquals(9, game1.error);
+		
+	}
+
+}
+
+
